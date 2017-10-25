65110858e6b903cf7b2d0711e7c34d8b0f61827f
Austin White <austingym@gmail.com>
2017-10-25 16:24:08 -0700

    Fixed up autoloading.

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,3 @@
 <?php
 
 require_once 'vendor/autoload.php';
-
-if (!function_exists('log_msg')) {
-    function log_msg($message, $class = '') {
-        echo (!empty($class)) ? strtoupper($class) . ": " . $message : $message;
-        echo "\n";
-    }
-}
